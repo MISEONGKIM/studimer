a72cc4e5a9d031c473e8347f344dbdfe483ddadd
MISEONGKIM <rlaaltjd46@jinhakapply.com>
2022-10-25 18:01:46 +0900

    fix : timer class 분리

--- lib/src/presentation/widgets/timer/timer_text_container.dart
+++ lib/src/presentation/widgets/timer/timer_text_container.dart
@@ -2,32 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:studimer/src/core/common/provider_consumer.dart';
 
 class TimerTextContainer extends StatelessWidget {
-  const TimerTextContainer({Key? key, required this.isRunning})
-      : super(key: key);
-  final bool isRunning;
+  const TimerTextContainer({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    final style = TextStyle(
-        fontSize: 70,
-        fontWeight: FontWeight.bold,
-        color: isRunning ? Colors.orangeAccent : Colors.white);
-    return TimerPrvdConsumer(
-        builder: (context, provider) => Container(
-            width: double.infinity,
-            margin: const EdgeInsets.only(top: 10),
-            padding: const EdgeInsets.all(10),
-            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text(provider.lpadZeroFormatter(provider.hour), style: style),
-              Text(
-                ' : ',
-                style: style,
-              ),
-              Text(provider.lpadZeroFormatter(provider.minuate), style: style),
-              Text(
-                ' : ',
-                style: style,
-              ),
-              Text(provider.lpadZeroFormatter(provider.second), style: style)
-            ])));
+    return TimerPrvdConsumer<>(builder: (context, provider) {
+      final style = TextStyle(
+          fontSize: 70,
+          fontWeight: FontWeight.bold,
+          color: provider.isRunning ? Colors.orangeAccent : Colors.white);
+
+      return Container(
+          width: double.infinity,
+          margin: const EdgeInsets.only(top: 10),
+          padding: const EdgeInsets.all(10),
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(provider.lpadZeroFormatter(provider.hour), style: style),
+            Text(
+              ' : ',
+              style: style,
+            ),
+            Text(provider.lpadZeroFormatter(provider.minuate), style: style),
+            Text(
+              ' : ',
+              style: style,
+            ),
+            Text(provider.lpadZeroFormatter(provider.second), style: style)
+          ]));
+    });
   }
 }
