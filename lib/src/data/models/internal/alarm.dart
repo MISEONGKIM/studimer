8cd0587a322b7addf86cb113916581528984f9a0
MISEONGKIM <rlaaltjd46@jinhakapply.com>
2022-11-15 16:48:41 +0900

    fix : vibration 1분 duration 추가

--- lib/src/data/models/internal/alarm.dart
+++ lib/src/data/models/internal/alarm.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
 import 'package:studimer/src/core/resources/type.dart';
 import 'package:vibration/vibration.dart';
@@ -6,23 +8,38 @@ class Alarm {
   final NoticeType alarmCode;
   final String alarmName;
   final bool isUse;
+  late Timer vibrationTimer;
   static List<Alarm> noticeList = [
-    const Alarm(alarmCode: NoticeType.vivration, alarmName: '진동', isUse: true),
-    const Alarm(alarmCode: NoticeType.sounds, alarmName: '소리', isUse: false)
+    Alarm(alarmCode: NoticeType.vivration, alarmName: '진동', isUse: true),
+    Alarm(alarmCode: NoticeType.sounds, alarmName: '소리', isUse: false)
   ];
 
-  const Alarm(
+  Alarm(
       {required this.alarmCode, required this.alarmName, required this.isUse});
 
-  startAlarm(NoticeType type) async {
+  void _startOneMinuateVibrate() {
+    int second = 60;
+    vibrationTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      second -= 1;
+      Vibration.vibrate(pattern: [500, 1000]);
+      if (second == 0) {
+        timer.cancel();
+      }
+    });
+  }
+
+  void startAlarm(NoticeType type) async {
     type == NoticeType.vivration
-        ? Vibration.vibrate(duration: 1000, amplitude: 1)
+        ? _startOneMinuateVibrate()
         : FlutterRingtonePlayer.playAlarm();
   }
 
-  stopAlarm(NoticeType type) async {
-    type == NoticeType.vivration
-        ? Vibration.cancel()
-        : FlutterRingtonePlayer.stop();
+  void stopAlarm(NoticeType type) async {
+    if (type == NoticeType.vivration) {
+      Vibration.cancel();
+      vibrationTimer.cancel();
+      return;
+    }
+    FlutterRingtonePlayer.stop();
   }
 }
